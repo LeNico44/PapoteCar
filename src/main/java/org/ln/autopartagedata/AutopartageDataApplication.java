fb386dd0c140f9605f477fae8ae99b6aab08efa7
LeNico44 <nicolas.houisse@epsi.fr>
2018-11-22 17:40:26 +0100

    init Project

---
+++ src/main/java/org/ln/autopartagedata/AutopartageDataApplication.java
@@ -0,0 +1,12 @@
+package org.ln.autopartagedata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutopartageDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutopartageDataApplication.class, args);
+    }
+}
