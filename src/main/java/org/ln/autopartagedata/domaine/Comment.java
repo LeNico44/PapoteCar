a0203a553d1690273ce32f22c546553a56b4c814
LeNico44 <nicolas.houisse@epsi.fr>
2018-11-28 23:01:43 +0100

    Pojo Comment & Step not finish !

--- src/main/java/org/ln/autopartagedata/domaine/Comment.java
+++ src/main/java/org/ln/autopartagedata/domaine/Comment.java
@@ -1,4 +1,92 @@
 package org.ln.autopartagedata.domaine;
 
-public class Comment {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+public class Comment implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_comment;
+    private String content;
+    private String title;
+    private Date date;
+    private RoadTrip roadTrip;
+    private User user;
+
+    public Comment() {
+    }
+
+    public Comment(Long id_comment, String content, String title, Date date, RoadTrip roadTrip, User user) {
+        this.id_comment=id_comment;
+        this.content=content;
+        this.title=title;
+        this.date=date;
+        this.roadTrip=roadTrip;
+        this.user=user;
+    }
+
+    public Long getId_comment() {
+        return id_comment;
+    }
+
+    public void setId_comment(Long id_comment) {
+        this.id_comment=id_comment;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content=content;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title=title;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date=date;
+    }
+
+    public RoadTrip getRoadTrip() {
+        return roadTrip;
+    }
+
+    public void setRoadTrip(RoadTrip roadTrip) {
+        this.roadTrip=roadTrip;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user=user;
+    }
+
+    @Override
+    public String toString() {
+        return "Comment{" +
+                "id_comment=" + id_comment +
+                ", content='" + content + '\'' +
+                ", title='" + title + '\'' +
+                ", date=" + date +
+                ", roadTrip=" + roadTrip +
+                ", user=" + user +
+                '}';
+    }
 }
--- src/main/java/org/ln/autopartagedata/domaine/Step.java
+++ src/main/java/org/ln/autopartagedata/domaine/Step.java
@@ -1,4 +1,136 @@
 package org.ln.autopartagedata.domaine;
 
-public class Step {
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
+
+@Entity
+public class Step implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_step;
+    private String startPoint;
+    private String endPoint;
+    private Date estimateStratTime;
+    private Date estimateEndTime;
+    private Boolean canceled;
+    private Date realStratTime;
+    private Date realEndTime;
+    private Set<User> passengers;
+    private RoadTrip roadTrip;
+
+    public Step() {
+    }
+
+    public Step(String startPoint, String endPoint, Date estimateStratTime, Date estimateEndTime, Boolean canceled, Date realStratTime, Date realEndTime, Set<User> passengers, RoadTrip roadTrip) {
+        this.startPoint=startPoint;
+        this.endPoint=endPoint;
+        this.estimateStratTime=estimateStratTime;
+        this.estimateEndTime=estimateEndTime;
+        this.canceled=canceled;
+        this.realStratTime=realStratTime;
+        this.realEndTime=realEndTime;
+        this.passengers=passengers;
+        this.roadTrip=roadTrip;
+    }
+
+    public Long getId_step() {
+        return id_step;
+    }
+
+    public void setId_step(Long id_step) {
+        this.id_step=id_step;
+    }
+
+    public String getStartPoint() {
+        return startPoint;
+    }
+
+    public void setStartPoint(String startPoint) {
+        this.startPoint=startPoint;
+    }
+
+    public String getEndPoint() {
+        return endPoint;
+    }
+
+    public void setEndPoint(String endPoint) {
+        this.endPoint=endPoint;
+    }
+
+    public Date getEstimateStratTime() {
+        return estimateStratTime;
+    }
+
+    public void setEstimateStratTime(Date estimateStratTime) {
+        this.estimateStratTime=estimateStratTime;
+    }
+
+    public Date getEstimateEndTime() {
+        return estimateEndTime;
+    }
+
+    public void setEstimateEndTime(Date estimateEndTime) {
+        this.estimateEndTime=estimateEndTime;
+    }
+
+    public Boolean getCanceled() {
+        return canceled;
+    }
+
+    public void setCanceled(Boolean canceled) {
+        this.canceled=canceled;
+    }
+
+    public Date getRealStratTime() {
+        return realStratTime;
+    }
+
+    public void setRealStratTime(Date realStratTime) {
+        this.realStratTime=realStratTime;
+    }
+
+    public Date getRealEndTime() {
+        return realEndTime;
+    }
+
+    public void setRealEndTime(Date realEndTime) {
+        this.realEndTime=realEndTime;
+    }
+
+    public Set<User> getPassengers() {
+        return passengers;
+    }
+
+    public void setPassengers(Set<User> passengers) {
+        this.passengers=passengers;
+    }
+
+    public RoadTrip getRoadTrip() {
+        return roadTrip;
+    }
+
+    public void setRoadTrip(RoadTrip roadTrip) {
+        this.roadTrip=roadTrip;
+    }
+
+    @Override
+    public String toString() {
+        return "Step{" +
+                "id_step=" + id_step +
+                ", startPoint='" + startPoint + '\'' +
+                ", endPoint='" + endPoint + '\'' +
+                ", estimateStratTime=" + estimateStratTime +
+                ", estimateEndTime=" + estimateEndTime +
+                ", canceled=" + canceled +
+                ", realStratTime=" + realStratTime +
+                ", realEndTime=" + realEndTime +
+                ", passengers=" + passengers +
+                ", roadTrip=" + roadTrip +
+                '}';
+    }
 }
